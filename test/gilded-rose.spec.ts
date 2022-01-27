1869f101e7592035e4f332ab3a6414172d7462f2
Deepashri Dali <Deepashri.Dali@softwire.com>
2022-01-27 13:12:11 +0000

    Part One added golden master test

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -3,10 +3,40 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+    it('should correspond to the golden master', function() {
+
+    const inputItems = [
+        new Item("Aged Brie", 12, 3),
+        new Item("Aged Brie", 8, 9),
+        new Item("Aged Brie", 5, 6),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 20, 10),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 8, 45),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 4, 48),
+        new Item("Sulfuras, Hand of Ragnaros",5,80),
+        new Item("Sulfuras, Hand of Ragnaros",10,50),
+        new Item("Sulfuras, Hand of Ragnaros",4,70),
+        new Item("Item1", 5, 2),
+        ]
+        const expectedOutputItems = [
+            new Item('Aged Brie', 8, 7 ),
+            new Item('Aged Brie', 4, 13 ),
+            new Item('Aged Brie', 1, 10 ),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 16, 14),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 4, 50),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 0, 50),
+            new Item('Sulfuras, Hand of Ragnaros', 5,  80 ),
+            new Item('Sulfuras, Hand of Ragnaros',  10,  50 ),
+            new Item('Sulfuras, Hand of Ragnaros', 4, 70 ),
+            new Item('Item1', 1, 0),
+        ]
+       const gildedRose = new GildedRose(inputItems);
+
+       gildedRose.updateQuality();
+       gildedRose.updateQuality();
+       gildedRose.updateQuality();
+       gildedRose.updateQuality();
+       
+        expect(gildedRose.items).to.deep.equal(expectedOutputItems);
     });
 
 });
